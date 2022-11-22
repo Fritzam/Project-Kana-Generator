5f5b4b0972803b55a90d3c570789a23028f55343
Michal Fritza <Tornak9@gmail.com>
2022-11-22 10:06:42 +0100

    Add additional signs.

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 //Array consisting of Hiragana strings that I'm currently learning.
 const hiragana = ['a', 'e', 'i', 'o', 'u', 'ka', 'ke', 'ki', 'ko', 'ku',
 'sa', 'se', 'shi', 'so', 'su', 'ta', 'te', 'chi', 'to','tsu',
-'na', 'ne', 'ni', 'no', 'nu'];
+'na', 'ne', 'ni', 'no', 'nu', 'za', 'ze', 'Zji', 'zo', 'Zzu', 'da', 'de', 'Dji', 'do', 'Dzu',
+'ha', 'he', 'hi', 'ho', 'fu', 'ma', 'me', 'mi', 'mo', 'mu', 'ya', 'yo', 'yu',
+'ra', 're', 'ri', 'ro', 'ru', 'wa', 'wo', 'n', 'ba', 'be', 'bi', 'bo', 'bu',
+'pa', 'pe', 'pi', 'po', ' pu'];
 
 //Array consisting of Hiragana signs that I'm currently learning.
 const hiragana_signs = ['あ', 'え', 'い', 'お', 'う', 'か', 'け', 'き', 'こ', 'く',
 'さ', 'せ', 'し', 'そ', 'す', 'た', 'て', 'ち', 'と', 'つ', 'な', 'ね',
-'に', 'の', 'ぬ'];
+'に', 'の', 'ぬ', 'ざ', 'ぜ', 'じ', 'ぞ', 'ず', 'だ', 'で', 'ぢ', 'ど', 'づ',
+'は', 'へ', 'ひ', 'ほ', 'ふ', 'ま', 'め', 'み', 'も', 'む',
+'や', 'ゆ', 'よ', 'ら', 'れ', 'り', 'ろ', 'る', 'わ', 'を', 'ん',
+'ば', 'べ', 'び', 'ぼ', 'ぶ', 'ぱ', 'ぺ', 'ぴ', 'ぽ', 'ぷ'];
 
 //Function that returns a randomly generated number from 0 to hiragana.length.
 function getRandomNumber(min, max) {
